6d3dfc851dc3b30e6d215ef6b87d900a7c4a35de
eunli <eunli99012@gmail.com>
2024-12-02 06:02:52 +0900

    Feat: 날씨 조회 api 추가

--- src/main/java/com/example/weather/controller/DiaryController.java
+++ src/main/java/com/example/weather/controller/DiaryController.java
@@ -1,14 +1,13 @@
 package com.example.weather.controller;
 
+import com.example.weather.domain.Diary;
 import com.example.weather.service.DiaryService;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class DiaryController {
@@ -18,11 +17,20 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
-    @PostMapping("/create/diary")
-    void createDiary(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-            @RequestBody String text
-            ) {
+    @PostMapping("/diary")
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+                     @RequestBody String text) {
         diaryService.createDiary(date, text);
+    }
+
+    @GetMapping("/diary")
+    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        return diaryService.readDiary(date);
+    }
+
+    @GetMapping("/diaries")
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+        return diaryService.readDiaries(startDate, endDate);
     }
 }
